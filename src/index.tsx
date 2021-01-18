9cc9691806694a7dc9210463685c2b290f3a6b95
Zain Hill <zainthedev@gmail.com>
2021-01-18 17:44:24 +0900

    Added info to .env file

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-//Firebase/reactfire
+// Firebase/reactfire
 import { FirebaseAppProvider } from "reactfire";
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
+}
 
 ReactDOM.render(
   <React.StrictMode>
