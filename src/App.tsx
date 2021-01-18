f08fb07e953311434c9db3c37ab2ca2fb961a539
Zain Hill <zainthedev@gmail.com>
2021-01-18 23:50:30 +0900

    Added css and main image

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import mainImage from './images/main.png'
+import { MainImageContainer, MainImage } from './styled-components/imageStyles'
+import './styles/reset.css';
+import './styles/main.css';
 
 const App = () => {
   return (
     <div className="App">
+      <MainImageContainer>
+        <MainImage src={mainImage}/>
+      </MainImageContainer>
     </div>
   );
 }
--- src/styled-components/imageStyles.ts
+++ src/styled-components/imageStyles.ts
@@ -1,11 +1,9 @@
-import mainImage from './images/main.jpeg'
 import styled from 'styled-components';
 
 export const MainImageContainer = styled.div`
     display: flex;
 `;
 
-export const MainImage = styled.image`
-    height: 500px;
-    src: ${mainImage}
+export const MainImage = styled.img`
+    width: 2000px;
 `;
