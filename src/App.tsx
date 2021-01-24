ed33a34ff25789f124bcc9397c1503c3b907916d
Zain Hill <zainthedev@gmail.com>
2021-01-24 16:29:13 +0900

    Added: Display of tooltip on click only

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,27 @@ import './styles/main.css';
 
 export default function App() {
   const [mousePosition, setMousePosition] = useState({x: 0, y: 0})
+  const [tagging, setTagging] = useState(false)
 
   const handleMousePosition = (e: React.MouseEvent): void => {
     setMousePosition({x: e.pageX, y: e.pageY});
   };
 
+  const handleTooltipDisplay = () => {
+    tagging ? setTagging(false) : setTagging(true)
+  }
+  const handleClick = (e: React.MouseEvent): void => {
+    handleTooltipDisplay()
+    handleMousePosition(e)
+  }
+
   return (
     <div className="App">
       <HeaderComponent />
+      {tagging === true &&(
       <TooltipComponent mousePosition={mousePosition}/>
-      <MainImageContainerComponent handleMousePosition={handleMousePosition} />
+      )}
+      <MainImageContainerComponent handleMousePosition={handleClick} />
     </div>
   );
 }
