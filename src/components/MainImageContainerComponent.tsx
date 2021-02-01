bffe3d628cf46a4b254fce72eb1e3ecc1461e2da
Zain Hill <zainthedev@gmail.com>
2021-02-01 18:53:30 +0900

    Changed: See previous commit

--- src/components/MainImageContainerComponent.tsx
+++ src/components/MainImageContainerComponent.tsx
@@ -1,5 +1,5 @@
 import { MainImageContainer, PS2 } from '../styled-components/imageStyles';
-import ps2Image from '../images/ps2Image.webp';
+import ps2Image from '../images/ps2Image.jpg';
 
 export type MainImageContainerProps = {
     handleClick: (e: React.MouseEvent) => void,
